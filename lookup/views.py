bb77fe8c77eb2761040c4e000c62c5699fdeeb42
Thomas <Thomas.Mattinzioli@gmail.com>
2021-02-22 19:43:09 +0100

    initial commit

---
+++ lookup/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create home HTML here. When you want to enter webpage, you send a "request".
+def home(request):
+	#This is our view.
+	return render(request, 'home.html', {})
+
+def about(request):
+	#This is our view.
+	return render(request, 'about.html', {})	
